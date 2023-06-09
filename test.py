48b6e9db3465e4926d8a0aa13fb56561a0f36b49
pajhe <pauljeronheinke@gmail.com>
2023-06-09 12:26:02 +0200

    partition monitoring added

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import psutil
 import time
 
 def monitor_system():
+    disk_paths = ['C:\\', 'E:\\']  # Example disk paths to monitor on Windows
+
     while True:
         # CPU usage
         cpu_percent = psutil.cpu_percent(interval=1)
@@ -15,11 +17,15 @@ def monitor_system():
         print(f"RAM Usage: {mem_used:.2f}MB / {mem_total:.2f}MB ({mem_percent}%)")
 
         # Disk usage
-        disk_usage = psutil.disk_usage('/')
-        disk_percent = disk_usage.percent
-        disk_used = disk_usage.used / 1024 / 1024 / 1024  # in GB
-        disk_total = disk_usage.total / 1024 / 1024 / 1024  # in GB
-        print(f"Disk Usage: {disk_used:.2f}GB / {disk_total:.2f}GB ({disk_percent}%)")
+        for path in disk_paths:
+            try:
+                disk_usage = psutil.disk_usage(path)
+                disk_percent = disk_usage.percent
+                disk_used = disk_usage.used / 1024 / 1024 / 1024  # in GB
+                disk_total = disk_usage.total / 1024 / 1024 / 1024  # in GB
+                print(f"Disk ({path}) Usage: {disk_used:.2f}GB / {disk_total:.2f}GB ({disk_percent}%)")
+            except FileNotFoundError as e:
+                print(f"Disk path '{path}' not found.")
 
         # Delay for 5 seconds
         time.sleep(5)
