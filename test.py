dc2365463a5a73fdc263928c8b99b744727cf04e
pajhe <pauljeronheinke@gmail.com>
2023-06-09 11:00:20 +0200

    monitoring every 5 seconds instead of once

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
 import psutil
+import time
 
 def monitor_system():
-    # CPU usage
-    cpu_percent = psutil.cpu_percent(interval=1)
-    print(f"CPU Usage: {cpu_percent}%")
+    while True:
+        # CPU usage
+        cpu_percent = psutil.cpu_percent(interval=1)
+        print(f"CPU Usage: {cpu_percent}%")
 
-    # RAM usage
-    mem = psutil.virtual_memory()
-    mem_percent = mem.percent
-    mem_used = mem.used / 1024 / 1024  # in MB
-    mem_total = mem.total / 1024 / 1024  # in MB
-    print(f"RAM Usage: {mem_used:.2f}MB / {mem_total:.2f}MB ({mem_percent}%)")
+        # RAM usage
+        mem = psutil.virtual_memory()
+        mem_percent = mem.percent
+        mem_used = mem.used / 1024 / 1024  # in MB
+        mem_total = mem.total / 1024 / 1024  # in MB
+        print(f"RAM Usage: {mem_used:.2f}MB / {mem_total:.2f}MB ({mem_percent}%)")
 
-    # Disk usage
-    disk_usage = psutil.disk_usage('/')
-    disk_percent = disk_usage.percent
-    disk_used = disk_usage.used / 1024 / 1024 / 1024  # in GB
-    disk_total = disk_usage.total / 1024 / 1024 / 1024  # in GB
-    print(f"Disk Usage: {disk_used:.2f}GB / {disk_total:.2f}GB ({disk_percent}%)")
+        # Disk usage
+        disk_usage = psutil.disk_usage('/')
+        disk_percent = disk_usage.percent
+        disk_used = disk_usage.used / 1024 / 1024 / 1024  # in GB
+        disk_total = disk_usage.total / 1024 / 1024 / 1024  # in GB
+        print(f"Disk Usage: {disk_used:.2f}GB / {disk_total:.2f}GB ({disk_percent}%)")
+
+        # Delay for 5 seconds
+        time.sleep(5)
+        print('\n')  # Add a new line for separation
 
 monitor_system()
