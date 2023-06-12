d17c55d46308135206ff4633431bc6d814ece425
pajhe <pauljeronheinke@gmail.com>
2023-06-12 11:25:17 +0200

    pipeline test 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 def monitor_system():
     disk_paths = ['C:\\', 'D:\\', 'E:\\']  # Example disk paths to monitor on Windows
-
+    #test pipeline  
     while True:
         # CPU usage
         cpu_percent = psutil.cpu_percent(interval=1)
