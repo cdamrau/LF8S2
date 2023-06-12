89010d9397596237d8d43158c556c86348d6b34d
pajhe <pauljeronheinke@gmail.com>
2023-06-12 12:43:18 +0200

    test 2 anpassungen, pipeline

--- tests/test_main_2.py
+++ tests/test_main_2.py
@@ -1,8 +1,11 @@
 import time
 import os
+import sys
 from datetime import datetime
 import pytest
-from ..main import monitor_system  # Import the monitor_system function from your main file
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from main import monitor_system
+
 
 @pytest.fixture
 def log_file():
